04f2f7481e9490a68e764389bd52200c4ad4e4f3
Florian Rusch <florianrusch92@gmail.com>
2015-06-23 00:19:48 +0200

    Fixing bug at the highscore table. the entries weren't sorted.

--- highscore.php
+++ highscore.php
@@ -1,5 +1,13 @@
 <?php
 
+function sortEntries($entry, $entry2) {
+    if ((int) $entry['score'][0] == (int) $entry2['score'][0])
+        return 0;
+
+    return ((int) $entry['score'][0] < (int) $entry2['score'][0]) ? 1 : -1;
+}
+
+
 $highscore = simplexml_load_file("highscore.xml");
 
 if ($_POST['score'] !== null) {
@@ -12,29 +20,22 @@ if ($_POST['score'] !== null) {
     $highscore->saveXML("highscore.xml");
 
 } else {
+    $highscoreArray = array();
+    foreach($highscore->entry as $entry)
+        $highscoreArray[] = $entry;
 
-    $highscoreArray = (array) $highscore;
     usort($highscoreArray, "sortEntries");
 
     $counter = 1;
-    foreach ($highscore->entry as $entry) {
+    foreach($highscoreArray as $entry) {
         echo "<tr>";
             echo "<td>" . $counter . "</td>";
             echo "<td>" . $entry['name'] . "</td>";
             echo "<td>" . $entry['score'] . "</td>";
             echo "<td>" . $entry['date'] . "</td>";
         echo "</tr>";
+
         $counter++;
     }
 
-}
-
-
-function sortEntries($entry, $entry2) {
-    if ($entry['score'] > $entry2['score'])
-        return 1;
-    else if ($entry['score'] < $entry2['score'])
-        return -1;
-
-    return 0;
 }
